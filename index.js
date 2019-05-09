fb04fbc58db29dcd27456439285aa6dbcbbf04a5
Shaunak Mistry <shaunak.mistry@looker.com>
2019-05-09 11:11:09 -0700

    Moved older code to index2.js, new code is based on newer documentation Slack, also includes the re-direct URL.

--- index.js
+++ index.js
@@ -1,58 +1,171 @@
 
-var request = require('request')
-var express = require('express')
+const request = require('request')
+const express = require('express')
+const fs = require('fs')
+const rp = require('request-promise')
+const markdown = require('markdown-it')()
+const helpers = require('./helpers');
 
 
 
 
+const clientId = '548287020533.625058625702';
+const clientSecret = '7f6727c0a96aa971058462b7207a0262';
 
-var clientId = '548287020533.625058625702';
-var clientSecret = '7f6727c0a96aa971058462b7207a0262';
+const signingSecret = '6b257fb3bece21f7cd83d918867c6c86';
 
-var signingSecret = '6b257fb3bece21f7cd83d918867c6c86';
+const verificationToken = 'vcdGbrlJQJ6EsWti9wYHPCYr';
 
-var verificationToken = 'vcdGbrlJQJ6EsWti9wYHPCYr';
+const app = express();
 
-var app = express();
+const app2 = express();
 
-var app2 = express();
 
-PORT=4390;
+
+PORT_NO=4390;
 PORT2=4391;
 
-app.listen(PORT, function () {
-    console.log("Server listening on: http://localhost:%s", PORT);
+// Start the express application
+const port = process.env.PORT || PORT_NO;
+app.listen(port, () => {
+    console.log(`server listening on port ${port}`)
 });
 
-app.get('/', function (req, res) {
-    res.send('Ngrok is working! - Path Hit: ' + request.url);
+
+
+/**
+ STEP 1: Initialize the OAuth Flow
+
+ Documentation: https://api.slack.com/docs/oauth
+ **/
+app.get('/install', (req, res) => {
+    let scopes = ['commands']
+    let params = {
+        client_id: process.env.SLACK_CLIENT_ID || clientId,
+        scope:scopes.join(' '),
+        //redirect_uri: process.env.SLACK_REDIRECT_URL,
+        redirect_uri: 'http://2745dedd.ngrok.io/redirect/command',
+        state: `unique_string_passed_back_upon_completion`
+    };
+    // add parameters to the redirect url, e.g.
+    // https://slack.com/oauth/authorize?client_id=<your-client-id>&scope=commands&redirect_uri=<your-redirect-url>&state=<unique-string>
+    let url = helpers.getUrlWithParams('https://slack.com/oauth/authorize', params);
+    return res.redirect(url)
 });
 
-app.get('/oauth', function(req, res) {
-    if (!req.query.code) {
-        res.status(500);
-        res.send({"Error": "Looks like we're not getting code."});
-        console.log("Looks like we're not getting code.");
-    } else {
-        // If it's there...
+/**
+ STEP 1 (alternative): Initialize the OAuth Flow through the "Add to Slack" button
+ Documentation: https://api.slack.com/docs/slack-button
+ **/
+app.get('/add-to-slack-button', (req, res) => {
+    // TODO: add your required scopes
+    let scopes = ['commands'];
 
-        // We'll do a GET call to Slack's `oauth.access` endpoint, passing our app's client ID,
-        // client secret, and the code we just got as query parameters.
-        request({
-            url: 'https://slack.com/api/oauth.access', //URL to hit
-            qs: {code: req.query.code, client_id: clientId, client_secret: clientSecret}, //Query string data
-            method: 'GET', //Specify the method
-        }, function (error, responce, body) {
-            if (error) {
-                console.log(error);
+    let params = {
+        client_id: process.env.SLACK_CLIENT_ID || clientId,
+        scope: scopes.join(' '),
+        redirect_uri: 'http://2745dedd.ngrok.io/redirect/command',
+        //redirect_uri: process.env.SLACK_REDIRECT_URL,
+        state: 'unique-string-to-be-passed-back-upon-completion'
+    };
 
-            } else {
-                res.json(body);
-            }
+    // add parameters to the redirect url, e.g.
+    // https://slack.com/oauth/authorize?client_id=<your-client-id>&scope=bot commands&redirect_uri=<your-redirect-url>&state=<unique-string>
+    let url = helpers.getUrlWithParams('https://slack.com/oauth/authorize', params);
+
+    // render the Slack Button
+    return res.send('<a href="'+url+'"><img alt=""Add to Slack"" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>')
+});
+
+
+app.get('/signin', function (req, res){
+    res.sendFile(__dirname + '/sign_in_with_slack.html')
+});
+
+
+/**
+ STEP 2: Retrieve API Access Token after Authorization Grant
+
+ Documentation: https://api.slack.com/methods/oauth.access
+ **/
+app.get('/redirect', (req, res) => {
+    // TODO: verify that `req.query.state` is the same as your App provided when the flow was initialized
+
+    console.log('In /redirect');
+    let params = {
+        client_id: clientId,
+        client_secret: clientSecret,
+        redirect_uri: 'http://2745dedd.ngrok.io/redirect/command',
+        //redirect_uri: process.env.SLACK_REDIRECT_URL,
+        code: req.query.code
+    };
+
+    return rp({
+        url: helpers.getUrlWithParams('https://slack.com/api/oauth.access', params),
+        method: 'GET'
+    })
+        .then(result => {
+            let slackData = JSON.parse(result);
+
+            if(!slackData) throw new Error('Invalid Slack API data received');
+            if(!slackData.ok) throw new Error(slackData.error);
+
+            // TODO: Store Access Tokens in your Database
+
+            return res.send(slackData)
         })
-    }
-
+        .catch(err => {
+            console.log(err);
+            return res.send({error: err.message})
+        })
 });
+
+
+app.get('/redirect/command', (req, res) => {
+    // TODO: verify that `req.query.state` is the same as your App provided when the flow was initialized
+
+    console.log('In /redirect/command');
+    let params = {
+        client_id: clientId,
+        client_secret: clientSecret,
+        redirect_uri: 'http://2745dedd.ngrok.io/redirect/command',
+        //redirect_uri: process.env.SLACK_REDIRECT_URL,
+        code: req.query.code
+    };
+
+    return rp({
+        url: helpers.getUrlWithParams('https://slack.com/api/oauth.access', params),
+        method: 'GET'
+    })
+        .then(result => {
+            let slackData = JSON.parse(result);
+
+            if(!slackData) throw new Error('Invalid Slack API data received');
+            if(!slackData.ok) throw new Error(slackData.error);
+
+            // TODO: Store Access Tokens in your Database
+
+            return res.send(slackData)
+        })
+        .catch(err => {
+            console.log(err);
+            return res.send({error: err.message})
+        })
+});
+
+
+/*app.get('/redirect/command', function(req, res) {
+    console.log('received command ' + req.query.state + ' Your ngrok tunnel is up and running!');
+    res.send( 'received command ' + req + ' Your ngrok tunnel is up and running!')
+});*/
+
+
+app.get('/redirect/command2', function(req, res) {
+    console.log('received command 2 ' + req.query.state + ' Your  bot is up and running!');
+    res.send( 'received command 2 ' + req + ' Your tunnel is up and running!')
+});
+
+
 
 app.get('/auth/redirect', function(req, res) {
    request(
@@ -73,23 +186,12 @@ app.get('/auth/redirect', function(req, res) {
    )
 });
 
-app.get('/addto', function (req, res){
-   res.sendFile(__dirname + '/add_to_slack.html')
-});
-
-app2.get('/signin', function (req, res){
-    res.sendFile(__dirname + '/sign_in_with_slack.html')
-});
-
-
 app.post('/command', function(req, res) {
+    console.log('received command ' + req + 'Your ngrok tunnel is up and running!')
     res.send( 'received command ' + req + 'Your ngrok tunnel is up and running!')
 });
 
 
-app2.listen(PORT2, function () {
-    console.log("Server 2 listening on: http://localhost:%s", PORT2);
-});
 
 
 
---
+++ index2.js
@@ -0,0 +1,105 @@
+
+const request = require('request')
+const express = require('express')
+const fs = require('fs')
+const rp = require('request-promise')
+const markdown = require('markdown-it')()
+
+
+
+
+
+const clientId = '548287020533.625058625702';
+const clientSecret = '7f6727c0a96aa971058462b7207a0262';
+
+const signingSecret = '6b257fb3bece21f7cd83d918867c6c86';
+
+const verificationToken = 'vcdGbrlJQJ6EsWti9wYHPCYr';
+
+const app = express();
+
+const app2 = express();
+const helpers = require('./helpers');
+
+
+PORT=4390;
+PORT2=4391;
+
+
+
+app.listen(PORT, function () {
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
+
+app.get('/', function (req, res) {
+    res.send('Ngrok is working! - Path Hit: ' + request.url);
+});
+
+app.get('/oauth', function(req, res) {
+    if (!req.query.code) {
+        res.status(500);
+        res.send({"Error": "Looks like we're not getting code."});
+        console.log("Looks like we're not getting code.");
+    } else {
+        // If it's there...
+
+        // We'll do a GET call to Slack's `oauth.access` endpoint, passing our app's client ID,
+        // client secret, and the code we just got as query parameters.
+        request({
+            url: 'https://slack.com/api/oauth.access', //URL to hit
+            qs: {code: req.query.code, client_id: clientId, client_secret: clientSecret}, //Query string data
+            method: 'GET', //Specify the method
+        }, function (error, responce, body) {
+            if (error) {
+                console.log(error);
+
+            } else {
+                res.json(body);
+            }
+        })
+    }
+
+});
+
+app.get('/auth/redirect', function(req, res) {
+   request(
+       {
+           url: 'https://slack.com/api/oauth.access',
+           qs: {code: req.query.code, client_id: clientId, client_secret: clientSecret},
+           method: 'GET',
+       }, function (error, responce, body) {
+           var JSONresponse = JSON.parse(body)
+           if (!JSONresponse.ok){
+               console.log(JSONresponse)
+               res.send("Error encountered: \n"+JSON.stringify(JSONresponse)).status(200).end()
+           }else{
+               console.log(JSONresponse)
+               res.send("Success!")
+           }
+       }
+   )
+});
+
+app.get('/addto', function (req, res){
+   res.sendFile(__dirname + '/add_to_slack.html')
+});
+
+
+
+app.post('/command', function(req, res) {
+    res.send( 'received command ' + req + 'Your ngrok tunnel is up and running!')
+});
+
+
+app2.listen(PORT2, function () {
+    console.log("Server 2 listening on: http://localhost:%s", PORT2);
+});
+
+app2.get('/signin', function (req, res){
+    res.sendFile(__dirname + '/sign_in_with_slack.html')
+});
+
+
+
+
+
